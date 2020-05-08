dc6e0f2114371697a64b97867e0d0a389b8dafc3
LeoKnox <LeoKnox@users.noreply.github.com>
2020-05-07 23:08:17 -0700

    Add files via upload

---
+++ list_test.py
@@ -0,0 +1,30 @@
+star1 = 'sol'
+star2 = 'alpha centauri'
+star3 = 'bernard'
+star4 = 'wolf 359'
+
+african = 'kilmanjaro'
+antartic = 'vinson'
+australia = 'puncak jaya'
+north_america = 'denali'
+pacific = 'mauna kea'
+south_america = 'aconcagua'
+
+stars = [
+    'sol',
+    'alpha centauri',
+    'bernard',
+    'wolf 359'
+]
+
+mountains = {
+    'african':'kilmanjaro',
+    'antartic':'vinson',
+    'australia':'puncak jaya',
+    'north_america':'denali',
+    'pacific':'mauna kea',
+    'south_america':'aconcagua'
+}
+
+print(f"Star Trek Star: {stars[3]}")
+print(f"Pacific mountain peak: {mountains['pacific']}")
