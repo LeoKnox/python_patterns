1678e6a672beffa483f33524d4d99734c89e65e2
LeoKnox <LeoKnox@users.noreply.github.com>
2020-05-08 23:19:48 -0700

    Add files via upload

---
+++ app3.py
@@ -0,0 +1,12 @@
+first_name = "conan"
+last_name = "the barbarian"
+note = "class: he's a barbarian"
+
+first_name_cap = first_name.capitalize()
+last_name_cap = last_name.capitalize()
+print(first_name_cap)
+print(last_name_cap)
+award_location = note.find('class: ')
+print(award_location)
+award_text = note[7:]
+print(award_text)
