0851923abd7b3fa760909dcfb9727b10f427ea84
LeoKnox <LeoKnox@users.noreply.github.com>
2020-05-08 22:35:32 -0700

    Add files via upload

---
+++ loop_test.py
@@ -0,0 +1,13 @@
+fruits = [
+    "lemon",
+    "apple",
+    "blueberry",
+    "orange",
+    "pear",
+    "grape",
+]
+
+print('Our fruit list:')
+for fruit in fruits:
+    print (f'We have {fruit}')
+    
