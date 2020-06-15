b91b3e1de7b70b04e1d0901741b7eb77a95c071b
LeoKnox <LeoKnox@users.noreply.github.com>
2020-06-14 22:32:54 -0700

    Add files via upload

--- test1.py
+++ test1.py
@@ -1,6 +1,11 @@
+import random
+
 def makeMap(len, width):
     for j in range(0, width):
+        z = random.random()*10
         print('*',end="")
+        if z // 2 == 1:
+            print('..', end='')
         for  i in range(0, len):
             print('.', end="")
         print('*')
