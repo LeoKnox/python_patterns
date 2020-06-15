834277eb3c93bda0f3bd6dacc26f5c115f21a49f
LeoKnox <LeoKnox@users.noreply.github.com>
2020-06-14 19:53:25 -0700

    Add files via upload

--- test1.py
+++ test1.py
@@ -1,7 +1,8 @@
 def makeMap(len, width):
     for j in range(0, width):
+        print('*',end="")
         for  i in range(0, len):
-            print('*', end="")
-        print("")
+            print('.', end="")
+        print('*')
 
 makeMap(5,5)
