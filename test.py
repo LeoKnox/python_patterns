05341122bf0d0da0676004444ef00684e84f471a
LeoKnox <LeoKnox@users.noreply.github.com>
2020-06-14 18:38:15 -0700

    Add files via upload

--- test.py
+++ test.py
@@ -5,5 +5,15 @@ def swapGreater(a):
     else:
         print(a)
 
+def quickSort(a):
+    if len(a) == 1:
+        print(a)
+    else:
+        print(a)
+        quickSort(a[0:len(a)-1])
+
 alist = [6, 5, 9, 3, 1, 8]
+blist = [6, 5]
+clist = [6]
+quickSort(alist)
 swapGreater(alist)
