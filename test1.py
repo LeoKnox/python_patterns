cab325cb09d61240c98710b046d16026a9c94c27
LeoKnox <LeoKnox@users.noreply.github.com>
2020-06-14 23:07:27 -0700

    Add files via upload

--- test1.py
+++ test1.py
@@ -4,9 +4,9 @@ def makeMap(len, width):
     for j in range(0, width):
         z = random.random()*15
         print('*',end="")
-        if z // 3 == 1:
+        if z < 6:
             len -= 1
-        elif z // 2 ==1:
+        elif z < 10:
             len += 1
         for  i in range(0, len):
             print('.', end="")
