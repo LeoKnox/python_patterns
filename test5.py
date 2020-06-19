5441af6813addbd818f7dbc68f8b81e64a7501d2
LeoKnox <LeoKnox@users.noreply.github.com>
2020-06-18 19:21:11 -0700

    Add files via upload

--- test5.py
+++ test5.py
@@ -1,13 +1,15 @@
 import random
 
 class Level:
-    def __init__(self, x_list):
-        self.x_list = x_list
+    x_list = []
+
+    def __init__(self):
+        pass
 
     def show(self):
         for i in self.x_list:
             print(i)
 
 x = [4,6,9,1]
-l = Level(x)
+l = Level()
 l.show()
