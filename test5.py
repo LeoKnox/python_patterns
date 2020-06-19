454748d4538d17a13c8470eb1a1e95be2c7abf6b
LeoKnox <LeoKnox@users.noreply.github.com>
2020-06-18 19:53:50 -0700

    Add files via upload

--- test5.py
+++ test5.py
@@ -9,7 +9,12 @@ class Level:
     def show(self):
         for i in self.x_list:
             print(i)
+    
+    def build(self):
+        self.x_list.append(7)
 
 x = [4,6,9,1]
 l = Level()
+l.show()
+l.build()
 l.show()
